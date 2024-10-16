a2e3598cc5c6a3df4f9d53ebe8cddae95fc1a6f3
bap2pecs <bap2pecs@proton.me>
2024-10-16 20:31:38 +0000

    resolve latest comments

--- scripts/set-bridge-ui-env.sh
+++ scripts/set-bridge-ui-env.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# Load environment variables from the top-level .env file
+set -a
+source $(pwd)/.env
+set +a
+
 # Path to the .bridge.env file
 ENV_FILE=$(pwd)/.bridge.env
 curl -s https://raw.githubusercontent.com/Snapchain/op-bridge-ui/main/.env.example > "${ENV_FILE}"
@@ -9,23 +14,34 @@ curl -s https://raw.githubusercontent.com/Snapchain/op-bridge-ui/main/.env.examp
 OP_DEPLOY_DIR=$(pwd)/.deploy
 ROLLUP_JSON="${OP_DEPLOY_DIR}/rollup.json"
 # Path to the OP deployments directory
-OP_DEPLOYMENTS_DIR=$(pwd)/optimism/packages/contracts-bedrock/deployments
+OP_DEPLOYMENTS_JSON_PATH=$(pwd)/.deploy/op-devnet-deployments-${L2_CHAIN_ID}.json
 
 echo "Updating .bridge.env file for OP Bridge UI..."
 # Extract contract addresses from the deployments .json file
-L1_STANDARD_BRIDGE_PROXY=$(jq -r '.L1StandardBridgeProxy' "$OP_DEPLOYMENTS_DIR/op-devnet-${L2_CHAIN_ID}.json")
-L1_OPTIMISM_PORTAL_PROXY=$(jq -r '.OptimismPortalProxy' "$OP_DEPLOYMENTS_DIR/op-devnet-${L2_CHAIN_ID}.json")
-L2_OUTPUT_ORACLE_PROXY=$(jq -r '.L2OutputOracleProxy' "$OP_DEPLOYMENTS_DIR/op-devnet-${L2_CHAIN_ID}.json")
+L1_STANDARD_BRIDGE_PROXY=$(jq -r '.L1StandardBridgeProxy' "$OP_DEPLOYMENTS_JSON_PATH")
+L1_OPTIMISM_PORTAL_PROXY=$(jq -r '.OptimismPortalProxy' "$OP_DEPLOYMENTS_JSON_PATH")
+L2_OUTPUT_ORACLE_PROXY=$(jq -r '.L2OutputOracleProxy' "$OP_DEPLOYMENTS_JSON_PATH")
+
+# TODO: 9545 is now hardcoded in the docker-compose.yml file and a few other places.
+#       we should make this more dynamic by defining a variable in the .env file
+L2_RPC_URL="http://localhost:9545"
 
 # Update .bridge.env file
-sed -i "s/^NEXT_PUBLIC_L1_CHAIN_ID=.*/NEXT_PUBLIC_L1_CHAIN_ID=$L1_CHAIN_ID/" "$ENV_FILE"
-sed -i "s/^NEXT_PUBLIC_L1_CHAIN_NAME=.*/NEXT_PUBLIC_L1_CHAIN_NAME=$L1_CHAIN_NAME/" "$ENV_FILE"
-sed -i "s/^NEXT_PUBLIC_L2_CHAIN_ID=.*/NEXT_PUBLIC_L2_CHAIN_ID=$L2_CHAIN_ID/" "$ENV_FILE"
-sed -i "s/^NEXT_PUBLIC_L2_CHAIN_NAME=.*/NEXT_PUBLIC_L2_CHAIN_NAME=$L2_CHAIN_NAME/" "$ENV_FILE"
+sed -i "s|^NEXT_PUBLIC_L1_CHAIN_ID=.*|NEXT_PUBLIC_L1_CHAIN_ID=$L1_CHAIN_ID|" "$ENV_FILE"
+sed -i "s|^NEXT_PUBLIC_L1_CHAIN_NAME=.*|NEXT_PUBLIC_L1_CHAIN_NAME=$L1_CHAIN_NAME|" "$ENV_FILE"
+sed -i "s|^NEXT_PUBLIC_L2_CHAIN_ID=.*|NEXT_PUBLIC_L2_CHAIN_ID=$L2_CHAIN_ID|" "$ENV_FILE"
+sed -i "s|^NEXT_PUBLIC_L2_CHAIN_NAME=.*|NEXT_PUBLIC_L2_CHAIN_NAME=$L2_CHAIN_NAME|" "$ENV_FILE"
 sed -i "s|^NEXT_PUBLIC_L1_RPC_URL=.*|NEXT_PUBLIC_L1_RPC_URL=$L1_RPC_URL|" "$ENV_FILE"
 sed -i "s|^NEXT_PUBLIC_L2_RPC_URL=.*|NEXT_PUBLIC_L2_RPC_URL=$L2_RPC_URL|" "$ENV_FILE"
-sed -i "s/^NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY=.*/NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY=$L1_STANDARD_BRIDGE_PROXY/" "$ENV_FILE"
-sed -i "s/^NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY=.*/NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY=$L1_OPTIMISM_PORTAL_PROXY/" "$ENV_FILE"
-sed -i "s/^NEXT_PUBLIC_L2_OUTPUT_ORACLE_PROXY=.*/NEXT_PUBLIC_L2_OUTPUT_ORACLE_PROXY=$L2_OUTPUT_ORACLE_PROXY/" "$ENV_FILE"
+sed -i "s|^NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY=.*|NEXT_PUBLIC_L1_STANDARD_BRIDGE_PROXY=$L1_STANDARD_BRIDGE_PROXY|" "$ENV_FILE"
+sed -i "s|^NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY=.*|NEXT_PUBLIC_L1_OPTIMISM_PORTAL_PROXY=$L1_OPTIMISM_PORTAL_PROXY|" "$ENV_FILE"
+sed -i "s|^NEXT_PUBLIC_L2_OUTPUT_ORACLE_PROXY=.*|NEXT_PUBLIC_L2_OUTPUT_ORACLE_PROXY=$L2_OUTPUT_ORACLE_PROXY|" "$ENV_FILE"
 sed -i "s|^NEXT_PUBLIC_L1_MULTICALL3_ADDRESS=.*|NEXT_PUBLIC_L1_MULTICALL3_ADDRESS=$NEXT_PUBLIC_L1_MULTICALL3_ADDRESS|" "$ENV_FILE"
+
+sed -i "s|^NEXT_PUBLIC_L1_EXPLORER_URL=.*|NEXT_PUBLIC_L1_EXPLORER_URL=$L1_EXPLORER_URL|" "$ENV_FILE"
+# TODO: we should change this when explorer is available
+sed -i "s|^NEXT_PUBLIC_L2_EXPLORER_URL=.*|NEXT_PUBLIC_L2_EXPLORER_URL=|" "$ENV_FILE"
+# TODO: we should change this when fault proof is enabled
+sed -i "s|^NEXT_PUBLIC_DISPUTE_GAME_FACTORY_PROXY=.*|NEXT_PUBLIC_DISPUTE_GAME_FACTORY_PROXY=|" "$ENV_FILE"
+
 echo "Updated .bridge.env file for OP Bridge UI"
