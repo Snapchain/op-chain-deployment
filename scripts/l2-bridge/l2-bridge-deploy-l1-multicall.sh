76a21eef2d4532c85a911e2a6c12680bd62ebbf3
bournezjc <bournezjc@gmail.com>
2024-10-21 18:55:05 +0800

    Chore: Check whether contract is already deployed (#19)

--- scripts/l2-bridge/l2-bridge-deploy-l1-multicall.sh
+++ scripts/l2-bridge/l2-bridge-deploy-l1-multicall.sh
@@ -9,10 +9,22 @@ set +a
 cd "$(pwd)/multicall"
 forge build
 
-# TODO: check if the contract is already deployed. skip if it is
-# - use cast to get the chain id using L1_RPC_URL
-# - curl https://raw.githubusercontent.com/mds1/multicall/main/deployments.json
-# - check if the chain id is in the deployments.json file
+CHAIN_ID=$(cast chain-id --rpc-url "${L1_RPC_URL}")
+
+DEPLOYED_URL=$(curl https://raw.githubusercontent.com/mds1/multicall/main/deployments.json | grep -A 3 "\"chainId\": ${CHAIN_ID},"  | grep "\"url\":" | awk -F'"' '{print $4}')
+if [ ! -z "${DEPLOYED_URL}" ]; then
+    # Extract the Ethereum address from the URL
+    DEPLOYED_ADDRESS=$(echo "$DEPLOYED_URL" | grep -oE '0x[a-fA-F0-9]{40}')
+    if [ -n "$DEPLOYED_ADDRESS" ]; then
+        echo "Contract is already deployed. Exiting."
+        # Update the .env file with the deployed address
+        sed -i.bak "s/^NEXT_PUBLIC_L1_MULTICALL3_ADDRESS=.*/NEXT_PUBLIC_L1_MULTICALL3_ADDRESS=${DEPLOYED_ADDRESS}/" ../.env
+        rm ../.env.bak
+        exit 0
+    else
+        echo "No Ethereum address found in the URL"
+    fi
+fi
 
 # Stream forge output in real-time
 forge create --rpc-url "${L1_RPC_URL}" --private-key "${L1_FUNDED_PRIVATE_KEY}" Multicall3 | tee forge_output.log
